98b26c2c0723f0a3f1101020557079b76d796eb4
ËnioDev <eniomejia57@gmail.com>
2023-08-30 08:14:09 -0600

    Instalacion de json-server

--- breakingbad/breaking-bad-app.js
+++ breakingbad/breaking-bad-app.js
@@ -21,8 +21,29 @@ export const BreakingbadApp = async( element ) => {
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderqUote = ( data) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authoLabel, nextQuoteButton );
+    }
+
+    nextQuoteButton.addEventListener('click', async() => {
+        
+    element.innerHTML = 'Loading...';
+
     const quote = await fetchQuote();
-    element.innerHTML = 'Tenemos data!!';
+    renderqUote( quote );
+  
+    });
+
+    fetchQuote()
+    .then( renderqUote ); 
 
 
 }
